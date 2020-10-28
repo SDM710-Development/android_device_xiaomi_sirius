ffab3976555110b7bd8d6339de57871d00eddd10
Alex Damaratski <alexeydomoratsky1@gmail.com>
2020-10-28 12:35:17 +0300

    sirius: Set device stock props

--- device-props.mk
+++ device-props.mk
@@ -1,9 +1,6 @@
-# Camera
+# Audio
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.sat.fallback.dist=45 \
-    persist.camera.sat.fallback.dist.d=5 \
-    persist.camera.sat.fallback.luxindex=405 \
-    persist.camera.sat.fallback.lux.d=20
+    ro.vendor.audio.soundfx.type=mi
 
 # Display density
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -13,4 +10,19 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.displayfeature.histogram.enable=true \
     ro.eyecare.brightness.threshold=3 \
-    ro.eyecare.brightness.level=8
+    ro.eyecare.brightness.level=8 \
+    ro.hist.brightness.threshold=5
+
+# Display postprocessing
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.display.enable_default_color_mode=0
+
+# Fingerprint
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.fp.fpc=true \
+    ro.hardware.fp.goodix=true
+
+# GNSS
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.flp=brcm \
+    ro.hardware.gps=brcm
