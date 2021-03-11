c81a97303ab3402d2cb475e2bf9cd1bc7d2fa86d
Ivan Vecera <ivan@cera.cz>
2021-03-11 12:08:09 +0100

    sirius: remove consumerir permissions
    ConsumerIR permissions file was moved to sdm710-common.
    
    Signed-off-by: Ivan Vecera <ivan@cera.cz>
    Change-Id: Iccb6b57554911cfb568b4b1de55e1687145d0de2

--- device.mk
+++ device.mk
@@ -26,7 +26,6 @@ PRODUCT_PACKAGES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml \
     frameworks/native/data/etc/android.hardware.sensor.barometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.barometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.hifi_sensors.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.hifi_sensors.xml \
 
