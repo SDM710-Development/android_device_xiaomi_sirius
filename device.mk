408da80b4a64fb54864c24c25d91ccd2c43a18b4
Alex Damaratski <alexeydomoratsky1@gmail.com>
2021-03-14 21:12:27 +0300

    sirius: Move IR to common tree
    All sdm710 devices have IR support, so we are defining that in common.
    
    Signed-off-by: Alex Damaratski <alexeydomoratsky1@gmail.com>
    Change-Id: If3b6a13d12f1e3a3f020f767905bfe2a0a8d9275

--- device.mk
+++ device.mk
@@ -47,10 +47,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths_wcd9340.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths_wcd9340.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
 
-# Consumer IR
-PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-service.xiaomi
-
 # Device fstab
 PRODUCT_PACKAGES += \
     fstab.qcom
